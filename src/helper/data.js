8dd59b7c3c21092b2d299e42756d17ccc6c700c1
RupeshDahal2 <rupeshdahal297@gmail.com>
2024-07-11 12:05:23 +0545

    Landing

---
+++ src/helper/data.js
@@ -0,0 +1,59 @@
+export const landingFooterLinks = [
+  {
+    id: "1",
+    title: "facebook",
+    link: "https://www.facebook.com",
+  },
+  {
+    id: "2",
+    title: "youtube",
+    link: "https://www.youtube.com",
+  },
+  {
+    id: "3",
+    title: "twitter",
+    link: "https://www.twitter.com",
+  },
+  {
+    id: "4",
+    title: "instagram",
+    link: "https://www.instagram.com",
+  },
+  {
+    id: "5",
+    title: "google",
+    link: "https://www.google.com",
+  },
+];
+export const landingHeaderLinks = [
+  {
+    id: "1",
+    title: "About",
+    link: "#",
+  },
+  {
+    id: "2",
+    title: "Tours",
+    link: "#",
+  },
+  {
+    id: "3",
+    title: "sale",
+    link: "#",
+  },
+  {
+    id: "4",
+    title: "Contact",
+    link: "#",
+  },
+  {
+    id: "5",
+    title: "Sign In",
+    link: "/signin",
+  },
+  {
+    id: "6",
+    title: "Sign Up",
+    link: "/signup",
+  },
+];
